42ad4333d6a69a77173c3c281b67b0f9cd076edd
Cute_Wisp <sweatpotato13@gmail.com>
2024-10-22 09:42:43 +0900

    feat: 🎸 add vercel web analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import "@/styles/globals.css";
 
+import { Analytics } from "@vercel/analytics/react";
+
 import Footer from "@/components/footer";
 import Topbar from "@/components/topbar";
 
@@ -14,6 +16,7 @@ export default function RootLayout({
                 <Topbar />
                 <main className="min-h-screen">{children}</main>
                 <Footer />
+                <Analytics />
             </body>
         </html>
     );
