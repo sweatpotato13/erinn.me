43ed568012e53ede57ee2bfbd0083bcd01d05940
Cute_Wisp <sweatpotato13@gmail.com>
2024-10-22 09:44:11 +0900

    feat: 🎸 add vercel speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 
-import { Analytics } from "@vercel/analytics/react";
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 import Footer from "@/components/footer";
 import Topbar from "@/components/topbar";
@@ -17,6 +18,7 @@ export default function RootLayout({
                 <main className="min-h-screen">{children}</main>
                 <Footer />
                 <Analytics />
+                <SpeedInsights />
             </body>
         </html>
     );
