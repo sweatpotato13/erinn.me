f718c89ef88ec52f04f3d828366c2278d46728ff
Cute_Wisp <sweatpotato13@gmail.com>
2024-10-27 00:08:23 +0900

    feat: 🎸 add metadata

---
+++ src/constant/metadata.ts
@@ -0,0 +1,22 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "Erinn.me",
+    description:
+        "A project utilizing Mabinogi's OpenAPI to proview market, horn and more <br><br>마비노기 오픈API를 활용한 경매장, 뿔피리 등 정보 제공",
+    applicationName: "erinn.me",
+    authors: { name: "Cute_Wisp", url: "https://github.com/sweatpotato13" },
+    keywords: [
+        "mabinogi",
+        "마비노기",
+        "openAPI",
+        "'경매장",
+        "거뿔",
+        "뿔피리",
+        "erinn.me",
+    ],
+    icons: "https://i.imgur.com/hdGJhCB.png",
+    openGraph: {
+        images: "https://i.imgur.com/hdGJhCB.png",
+    },
+};
