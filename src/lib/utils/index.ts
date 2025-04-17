8d333eefe6e9a0919b962e3f019b86bdda69de38
Cute_Wisp <sweatpotato13@gmail.com>
2025-04-17 11:46:53 +0900

    refactor: 💡 Add error handling for items not found in item list

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -2,5 +2,9 @@ import { AllItemList } from "@/constant/all-item-list";
 
 export function getItemImageUrl(itemName: string): string {
     const item = AllItemList.find(item => item.name === itemName);
-    return `/api/item-image?id=${item?.id}`;
+    if (!item) {
+        console.warn(`Item not found: ${itemName}`);
+        return `/api/item-image?id=1000`;
+    }
+    return `/api/item-image?id=${item.id}`;
 }
